7f4e1812b21d8b9625bf9e065849a09063a1db98
SrinivasRajaraman <rsri1011@gmail.com>
2017-05-03 12:19:08 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        public int DivideTest(int n, int d)
+        {
+            if (d == 0)
+                return -1;
+
+            return (n / d);
+        }
+
+        public int GetMax(int[] arr)
+        {
+            if (arr.Length == 0)
+                return arr[0];
+
+            int maxElement = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (maxElement < arr[i])
+                    maxElement = arr[i];
+            }
+
+            return maxElement;
+        }
+
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            int[] arr = { 1, 2, 3 };
+            int element = obj.GetMax(arr);
+            Console.WriteLine(element);
+            Console.ReadKey();
+        }
+
+       
+    }
+}
---
+++ TestClass1.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace ConsoleApplication2
+{
+    [TestFixture]
+    class TestClass1
+    {
+        [Test]
+        public void DivideTwoNumbers()
+        {
+            Program sut = new Program();
+            int expectedResult = sut.DivideTest(7, 8);
+            Assert.That(expectedResult, Is.EqualTo(0));
+
+            int expectedResult1 = sut.DivideTest(9, 8);
+            Assert.That(expectedResult1, Is.EqualTo(1));
+
+            int expectedResult2 = sut.DivideTest(8, 8);
+            Assert.That(expectedResult2, Is.EqualTo(1));
+
+            int expectedResult3 = sut.DivideTest(8, 0);
+            Assert.That(expectedResult3, Is.EqualTo(-1));
+
+            int[] iarray = new int[] { 2, 1, 3 };
+            int maxElement1 = sut.GetMax(iarray);
+            Assert.That(maxElement1, Is.EqualTo(3));
+
+            int[] iarray1 = new int[] { 1, 5, 9, 2, 3, 7, 8 };
+            int maxElement2 = sut.GetMax(iarray);
+            Assert.That(maxElement2, Is.EqualTo(9));
+        }
+
+    }
+}
